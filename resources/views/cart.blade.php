2b13f4307dc08a3838700e1d0fac9f5b81a36d52
marlontamo2 <marlontamo2@gmail.com>
2024-09-11 21:44:15 +0800

    added layout file

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,37 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Cart</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-
-
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-        <div class="container">
-            <a class="navbar-brand h3 bw-bold" href="#">MarkEd Store</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="http://127.0.0.1:8000">Shop</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/cart">Cart({{count($items)}})</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+@extends('layout.app')
+@section('content')
     <!-- cart + summary -->
     <section class="bg-light my-5">
         <div class="container">
@@ -152,7 +120,7 @@
 
                             <div class="mt-3">
                                 <a href="#" class="btn btn-success w-100 shadow-0 mb-2">
-                                    Make Purchase
+                                Checkout
                                 </a>
                                 <a href="{{ route('shop')}}" class="btn btn-light w-100 border mt-2">
                                     Back to shop
@@ -165,7 +133,7 @@
                 <!-- summary -->
             </div>
         </div>
-    </section>
+    @stop
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
